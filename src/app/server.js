19b48da3e43bc5f5c1feb9308f6aa41a3e78ce4f
CoraNers <cners1@live.maryville.edu>
2021-04-10 20:32:25 -0400

    Changes for Heroku. Becoming a pain...

--- src/app/server.js
+++ src/app/server.js
@@ -1,8 +1,16 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = process.env.PORT || 3001
+//Install express server
+const express = require('express');
+const path = require('path');
 
-app.use('/', express.static(path.join(__dirname, 'dist')))
+const app = express();
 
-app.listen(port, () => console.log("Listening on Port", port)) 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/lets-multiply'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/lets-multiply/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
