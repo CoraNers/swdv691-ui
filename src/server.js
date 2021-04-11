6fb027a8183b720762aaeec9169009e69e5da3bc
CoraNers <cners1@live.maryville.edu>
2021-04-11 13:59:10 -0400

    heroku

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/lets-multiply'));
+app.use(express.static(__dirname + '../dist/lets-multiply'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/lets-multiply/index.html'));
+res.sendFile(path.join(__dirname+'../dist/lets-multiply/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
