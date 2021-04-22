1c6956f6c6d2c871fd35ebe92759be7de70753e0
CoraNers <cners1@live.maryville.edu>
2021-04-22 19:54:32 -0400

    Cleanup login component spec imports.

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,23 +1,5 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterTestingModule } from '@angular/router/testing';
-import { DataServiceProvider } from 'src/providers/data-service/data-service';
-import { CompletedGameplayComponent } from '../completed-gameplay/completed-gameplay.component';
-import { GameplayComponent } from '../gameplay/gameplay.component';
-import { HistoryDetailComponent } from '../history-detail/history-detail.component';
-import { HistoryComponent } from '../history/history.component';
-import { WelcomeComponent } from '../welcome/welcome.component';
-import { LoginComponent } from './login.component';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { of } from 'rxjs';
-
+import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
