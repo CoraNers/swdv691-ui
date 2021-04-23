598c14fcd984430cb68ad7298acac3eb172d4f6d
CoraNers <cners1@live.maryville.edu>
2021-04-22 20:26:03 -0400

    History Detail Component Specs

--- src/app/history-detail/history-detail.component.spec.ts
+++ src/app/history-detail/history-detail.component.spec.ts
@@ -1,56 +1,56 @@
-import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterTestingModule } from '@angular/router/testing';
-import { CompletedGameplayComponent } from '../completed-gameplay/completed-gameplay.component';
-import { GameplayComponent } from '../gameplay/gameplay.component';
-import { HistoryComponent } from '../history/history.component';
-import { LoginComponent } from '../login/login.component';
-import { WelcomeComponent } from '../welcome/welcome.component';
 import { HistoryDetailComponent } from './history-detail.component';
 
 describe('HistoryDetailComponent', () => {
   let component: HistoryDetailComponent;
-  let fixture: ComponentFixture<HistoryDetailComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        FormsModule,
-        MatGridListModule,
-        MatFormFieldModule,
-        MatDialogModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        MatSnackBarModule,
-        MatInputModule 
-      ],
-      declarations: [ 
-        LoginComponent, 
-        WelcomeComponent,
-        HistoryComponent,
-        GameplayComponent,
-        HistoryDetailComponent,
-        CompletedGameplayComponent
-      ],
-    })
-    .compileComponents();
-  });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HistoryDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new HistoryDetailComponent();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  describe('goHome', () => {
+    it('should set doGoHome to true', () => {
+      expect(component.doGoHome).toEqual(false);
+      component.goHome();
+      expect(component.doGoHome).toEqual(true);
+    });
+  });
+
+  describe('goToUserHistory', () => {
+    it('should set doGoBackToHistory to true', () => {
+      expect(component.doGoBackToHistory).toEqual(false);
+      component.goToUserHistory();
+      expect(component.doGoBackToHistory).toEqual(true);
+    });
+  });
+
+  describe('parseDate', () => {
+    it('should return the date part that is expected (minus timezone info)', () => {
+      let retVal = component.parseDate('Fri Apr 09 2021 19:40:02 GMT-0400 (Eastern Daylight Time)');
+      expect(retVal).toEqual('Fri Apr 09 2021 19:40:02');
+    });
+  });
+
+  describe('calculatePercentCorrect', () => {
+    it('should return the value to 2 decimal points', () => {
+      let retVal = component.calculatePercentCorrect(6);
+      expect(retVal).toEqual('50.00');
+    });
+
+    it('should return the value to 2 decimal points when number passed in is zero', () => {
+      let retVal = component.calculatePercentCorrect(0);
+      expect(retVal).toEqual('0.00');
+    });
+  });
+
+  describe('parseOutQuestionAndAnswer', () => {
+    it('should return the question only when returnQuestionOnly is true', () => {
+      let retVal = component.parseOutQuestionAndAnswer('1 x 1 = 4', true);
+      expect(retVal).toEqual('1 x 1');
+    });
+
+    it('should return the question only when returnQuestionOnly is false', () => {
+      let retVal = component.parseOutQuestionAndAnswer('1 x 1 = 4', false);
+      expect(retVal).toEqual('4');
+    });
+  });
 });
--- src/app/history-detail/history-detail.component.ts
+++ src/app/history-detail/history-detail.component.ts
@@ -43,7 +43,7 @@ export class HistoryDetailComponent implements OnInit {
   parseDate(dateToParse) {
     let regex = /(.* .*:\d\d )/;
     let dateParts = regex.exec(dateToParse);
-    return dateParts[0]; 
+    return dateParts[0].trim(); 
   }
 
   calculatePercentCorrect(numCorrect) {
